06a05abaae3d13a6b2eb68364df4d3cb8ad98a13
Karpagapriya <akpriyakabileshwar@gmail.com>
2024-04-01 11:32:59 +0530

    Update Contact.js
    netlify update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -74,8 +74,8 @@ export default function Contact() {
           name="contact" 
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
-          data-netlify="true"
-        netlify >
+          netlify="true"
+          data-netlify="true">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Let's Collaborate
           </h2>
